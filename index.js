e5d3e77c57d726c632817f68a0541670c503e9f9
jamil <jamilhossain3251@gmail.com>
2023-05-18 01:42:23 +0600

    mongodb install and connect

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 app.use(express.json())
 app.use(cors());
 
-app.get('/' , (req,res) => {
-    res.send('hello')
-})
+
+
+const uri = "mongodb+srv://assignment-11:cNQwwIL4JTqdlwYN@cluster0.qcnne9d.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    client.connect();
+
+    const db = client.db('assignment-11').collection('collection0')
+
+
+    app.get('/' , async(req,res) => {
+        const result = await db.find().toArray()
+        res.send(result)
+    })
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.listen(process.env.PORT || 3000, () => {console.log('listening')})
