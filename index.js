06f6be5dd23a0e0f82cc4e3d76968a1f393d9a0f
jamil <jamilhossain3251@gmail.com>
2023-05-19 08:25:14 +0600

    toys params fix

--- index.js
+++ index.js
@@ -35,64 +35,33 @@ async function run() {
 
     app.get('/category' , async(req,res) => {
         let query = {} ;
-
-        // console.log(req.query.email)
         query = {category : req.query.category}
         const result = await db.find(query).limit(6).toArray()
-
         res.send(result)
         console.log(result)
     })
 
     app.get('/my-toys' , async(req,res) => {
-
-    
-            let query = {} ;
-
-            // console.log(req.query.email)
-
-            query = {sellerEmail : req.query.email}
-            const result = await db.find(query).toArray()
-            res.send(result)
-            // console.log(result)
-            // console.log(req.query.email)
-
+        let query = {} ;
+        query = {sellerEmail : req.query.email}
+        const result = await db.find(query).toArray()
+        res.send(result)
     })
 
     app.get('/my-toys/sort', async(req, res) => {
-
         let query = {} ;
         query = {sellerEmail : req.query.email}
         const result = await db.find(query).sort({price:parseInt(req.query.sorting)}).toArray()
         res.send(result);
-      })
+    })
 
-
-
-
-
-
-    app.put('/' , async(req,res) => {
-        const filter = {_id : new ObjectId(req.body.id)}
-        const options = { upsert : true }
-        const update = {
-          $set: {
-            name : req.body.name ,
-            price : req.body.price,
-            quantity : req.body.quantity,
-            description :req.body.description ,
-            rating : req.body.rating
-          }
-        }
-        console.log(update)
-        const result = await db.updateOne(filter,update,options)
+    app.get('/all-toys/:id' , async(req,res) => {
+        const query = {_id : new ObjectId(req.params.id)}
+        const result = await db.find(query).toArray()
         res.send(result)
-      })
-  
+    })
 
-    
     app.post('/addtoys' , async(req,res) => {
-
         const data = {
             name : req.body.name ,
             image: req.body.image,
@@ -104,41 +73,32 @@ async function run() {
             sellerName : req.body.sellerName,
             sellerEmail : req.body.sellerEmail,
         }
-        
         const result =await db.insertOne(data)
         res.send(result)
-
     })
 
-
-
-
-
+    app.put('/' , async(req,res) => {
+        const filter = {_id : new ObjectId(req.body.id)}
+        const options = { upsert : true }
+        const update = {
+            $set: {
+            name : req.body.name ,
+            price : req.body.price,
+            quantity : req.body.quantity,
+            description :req.body.description ,
+            rating : req.body.rating
+            }
+        }
+        console.log(update)
+        const result = await db.updateOne(filter,update,options)
+        res.send(result)
+    })
+  
     app.delete('/my-toys' , async(req,res) => {
         const query = {_id : new ObjectId(req.body.data)}
         const result = await db.deleteOne(query)
         res.send(result)
     })
-
-    // app.post('/' , async(req,res) => {
-    //     console.log('called')
-    //     const result = await db.find().limit(20+Number(req.body.value)).toArray()
-    //     res.send(result)
-    //     // console.log(result)
-    // })
-
-    app.get('/all-toys/:id' , async(req,res) => {
-
-        const query = {_id : new ObjectId(req.params.id)}
-        const result = await db.find(query).toArray()
-
-        console.log(result)
-        res.send(result)
-
-    })
-
-
-
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
